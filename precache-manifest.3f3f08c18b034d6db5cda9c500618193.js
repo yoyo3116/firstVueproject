de2266cd6848becbd74be6ff298a8f35540e3c2b
Sean <yoyo4116@gmail.com>
2021-08-10 14:42:11 +0800

    deploy

---
+++ precache-manifest.3f3f08c18b034d6db5cda9c500618193.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3974cc0d76ca8d0b3d7f",
+    "url": "/firstVueproject/css/app.ecd95060.css"
+  },
+  {
+    "revision": "6068b46cc0a91bc2cd17",
+    "url": "/firstVueproject/css/chunk-vendors.b8d09d9d.css"
+  },
+  {
+    "revision": "cc9bb50b3bd3b2bb5b01397a4e046a9e",
+    "url": "/firstVueproject/icon.ico"
+  },
+  {
+    "revision": "5cd9b0e2d97b3eb2bdf452e5c0287df0",
+    "url": "/firstVueproject/icon.png"
+  },
+  {
+    "revision": "4e3de1d371d30cf7aef8f91662cd8f43",
+    "url": "/firstVueproject/img/logo.4e3de1d3.png"
+  },
+  {
+    "revision": "9a6edf889353ec3d7e77e2ac313c23a1",
+    "url": "/firstVueproject/index.html"
+  },
+  {
+    "revision": "168df00a5bf941c2f536",
+    "url": "/firstVueproject/js/about.8b141152.js"
+  },
+  {
+    "revision": "3974cc0d76ca8d0b3d7f",
+    "url": "/firstVueproject/js/app.21c12100.js"
+  },
+  {
+    "revision": "6068b46cc0a91bc2cd17",
+    "url": "/firstVueproject/js/chunk-vendors.f94d4e53.js"
+  },
+  {
+    "revision": "d606c6529683216a708f2401db28ce16",
+    "url": "/firstVueproject/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/firstVueproject/robots.txt"
+  }
+]);
